1fd8ce0d3f698c8ad23bb04e0012aa8ba8239176
Niko Vertovec <niko.vertovec@outlook.com>
2016-07-14 16:01:55 +0200

    created main Project to bring everything together

---
+++ poc/LPS/LPS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LPS
+//
+//  Created by Niko Vertovec on 14/07/16.
+//  Copyright © 2016 Niko Vertovec. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
