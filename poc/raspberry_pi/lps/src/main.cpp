5a7cd49b0dba7f830d0a46250abaf1d8989e75f1
nikovert <niko.vertovec@outlook.com>
2016-08-04 11:14:17 +0200

    minor revision

--- poc/raspberry_pi/lps/src/main.cpp
+++ poc/raspberry_pi/lps/src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "ar_drone/drone.h"
 #include "detect_marker/location.h"
+#include "arucodrone/arucodrone.h"
 
 using namespace std;
 
@@ -19,16 +20,16 @@ using namespace std;
 // --------------------------------------------------------------------------
 int main(int argc, char **argv){
 	ArucoDrone drone;
-	drone.drone_location;
+	cv::Point3d point = drone.drone_location;
 
-	if(argc > 1){
-		if(strcmp(argv[1], "test_connection") == 0){
-			cout << "testing connection, drone will activate for 3 seconds" << endl;
-			testconnection();
-			cout << "finished!"<< endl;
-			return 0;
-		}
-	}
+//	if(argc > 1){
+//		if(strcmp(argv[1], "test_connection") == 0){
+//			cout << "testing connection, drone will activate for 3 seconds" << endl;
+//			testconnection();
+//			cout << "finished!"<< endl;
+//			return 0;
+//		}
+//	}
 
 	while(1){
 		drone.fly();
